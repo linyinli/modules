e46fe6510ad58839f7fee73a56d66409c589ad27
Yinlin Li <liyinlin926@gmail.com>
2023-07-10 18:44:34 +0800

    Update main.tf

--- server/main.tf
+++ server/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    ssh = {
+      source = "loafoe/ssh"
+    }
+  }
+}
+
 resource "ssh_resource" "server" {
   host        = var.host
   port        = var.port
