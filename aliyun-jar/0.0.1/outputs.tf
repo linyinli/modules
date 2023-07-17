cafe51f3277f008c652c05f7a48cfa4a400ec2f5
Yinlin Li <liyinlin926@gmail.com>
2023-07-17 17:35:17 +0800

    Update outputs.tf

--- aliyun-jar/0.0.1/outputs.tf
+++ aliyun-jar/0.0.1/outputs.tf
@@ -5,3 +5,8 @@ output "service_port" {
 output "endpoint_webservice" {
   value = "http://${alicloud_instance.example.public_ip}:${var.service_port}"
 }
+
+output "dependsOn" {
+  value = var.dependsOn
+  description = "Service dependsOn"
+}
