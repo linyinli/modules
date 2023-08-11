8aaf9c2ee1af518b87d1c4aa4beb7f4a646fc6f7
Yinlin Li <liyinlin926@gmail.com>
2023-08-11 14:15:55 +0800

    Update main.tf

--- alicloud-ons/main.tf
+++ alicloud-ons/main.tf
@@ -1,10 +1,10 @@
 resource "alicloud_ons_instance" "example" {
-    name   = var.name
+    instance_name   = var.instance_name
     remark = "default_ons_instance_remark"
 }
 
 # @label "实例名"
-variable "name" {
+variable "instance_name" {
     description = "The ID of the image used to launch the ECS instance"
     default = "onsInstanceName"
 }
